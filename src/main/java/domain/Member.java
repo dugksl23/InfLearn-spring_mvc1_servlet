6a763ec066499a213270a7d85e52b69043e434c0
dugksl23 <dugksl23@gmail.com>
2021-12-07 18:04:26 +0900

    feat : 회원 가입 servlet 및 회원 조회 servlet 작성 및 view 반환.

--- src/main/java/domain/Member.java
+++ src/main/java/domain/Member.java
@@ -2,10 +2,12 @@ package domain;
 
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class Member {
 
     private Long id;
---
+++ src/main/java/hello/servlet/web/MemberSaveServlet.java
@@ -0,0 +1,51 @@
+package hello.servlet.web;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import domain.Member;
+import domain.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name="memberSaveServlet", urlPatterns = "/servlet/members/save")
+public class MemberSaveServlet extends HttpServlet {
+
+//    @Autowired
+    private ObjectMapper objectMapper = new ObjectMapper();
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        ServletInputStream inputStream = req.getInputStream();
+        Member member = objectMapper.readValue(inputStream, Member.class);
+
+
+        //Member member = new Member(req.getParameter("username"), Long.parseLong(req.getParameter("age")));
+        member = memberRepository.save(member);
+
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("utf-8");
+
+        PrintWriter w = resp.getWriter();
+        w.write("<html>\n" +
+                "<head>\n" +
+                " <meta charset=\"UTF-8\">\n" + "</head>\n" +
+                "<body>\n" +
+                "성공\n" +
+                "<ul>\n" +
+                "    <li>id="+member.getId()+"</li>\n" +
+                "    <li>username="+member.getUsername()+"</li>\n" +
+                " <li>age="+member.getAge()+"</li>\n" + "</ul>\n" +
+                "<a href=\"/index.html\">메인</a>\n" + "</body>\n" +
+                "</html>");
+
+    }
+}
